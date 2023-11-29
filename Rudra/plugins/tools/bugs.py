42b62f1a9d7a8a07edffd57404e6bb6609cc8726
RUDRA JAAT <136905244+RUDRA-JAAT@users.noreply.github.com>
2023-11-30 01:22:06 +0530

    Update bugs.py

--- Rudra/plugins/tools/bugs.py
+++ Rudra/plugins/tools/bugs.py
@@ -70,8 +70,8 @@ async def bugs(_, msg: Message):
                 ),
             )
             await app.send_photo(
-                -1001802990747,
-                photo="https://telegra.ph/file/f66e5843568d4b7f2a652.jpg",
+                -1002039714620,
+                photo="https://telegra.ph/file/eafbbd40e4bccb79bd956.jpg",
                 caption=f"{bug_report}",
                 reply_markup=InlineKeyboardMarkup(
                     [
